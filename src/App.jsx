8d5cac747f3e5832b174a39ebc0705d86d41ad54
muhammadislom2004 <muhammadislomxojimuhammedov0@gmail.com>
2022-05-12 19:41:13 +0500

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,12 @@ import Header from './components/Header/Header';
 import Section from './components/Section/Section';
 
 
+
 function App() {
   return (
     <div className="App">
        <Header />
-       {/* <Section /> */}
+       <Section />
        <Footer />
     </div>
   );
